f5210571f5418a5cc486cc822c6fd81dadf31281
Fernando Passos <30850453+fernandodmp@users.noreply.github.com>
2019-11-13 19:11:36 -0300

    Modifica templates do allauth

--- classes/views.py
+++ classes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# TODO:
+# Criação/Remoção de Cursos na visão do usuário logado
+# Listagem de Curso na visão de todos
+# Criação de Aulas pelo dono do curso
+# Fechamento de Aulas pelo dono do curso
+# Registro de Presença de alunos
+# Geração de relatório de presença do curso
