a8f53a118678310d097ae63a24e81ed8bc1a17d0
Fernando Passos <30850453+fernandodmp@users.noreply.github.com>
2019-11-20 11:16:34 -0300

    Reorganiza o código

--- classes/views.py
+++ classes/views.py
@@ -37,6 +37,68 @@ def course_page(request, id):
 def generate_auth_code():
     return ''.join([random.choice(string.ascii_letters + string.digits) for n in range(6)])
 
+@login_required
+def subscribe_on_course(request, course_id):
+    course = get_object_or_404(models.Course, pk=course_id)
+    if request.user in course.participants.all():
+        return render(request, 'errors/already_in.html', status=400)
+    if request.method == "POST":
+        form = forms.SubscribeForm(request.POST)
+        if(form.is_valid()):
+            key = form.cleaned_data['course_key']
+            if key == course.access_key:
+                course.participants.add(request.user)
+                return redirect('course', id=course.id)
+            else:
+                return render(request, 'errors/wrong_key.html', status=400)
+
+@login_required
+def course_report(request, id):
+    course = get_object_or_404(models.Course, pk=id)
+    if request.user == course.owner:
+        participants = course.participants.all()
+        lessons = course.lesson_set.all()
+        ctx = {'participants': participants, 'lessons': lessons, 'course':course}
+        return render(request, 'classes/course_report.html', context = ctx)
+    else:
+        return render(request, 'errors/unauthorized.html', status=401)
+
+@login_required
+def new_course(request):
+    if request.method == "POST":
+        form = forms.CourseCreation(request.POST)
+        if(form.is_valid()):
+            course = form.save(commit=False)
+            course.owner = request.user
+            course.save()
+            return redirect('course', id=course.id)
+    else:
+        form = forms.CourseCreation()
+        return render(request, 'classes/create_course.html', context={'form':form})
+
+@login_required
+def my_courses(request):
+    owned_courses = request.user.owner.all()
+    subscribed_courses = request.user.participants.all()
+    ctx = {"owned": owned_courses, "subscribed": subscribed_courses}
+    return render(request, 'classes/my_courses.html', context=ctx)
+
+
+@login_required
+def delete_course(request, id):
+    course = get_object_or_404(models.Course, pk=id)
+    if request.user == course.owner:
+        course.delete()
+        return redirect('my_courses')
+    else:
+        return render(request, 'errors/unauthorized.html', status=401)
+
+@login_required
+def unsubscribe(request, id):
+    course = get_object_or_404(models.Course, pk=id)
+    course.participants.remove(request.user)
+    return redirect('my_courses')
+
 
 @login_required
 def add_lesson(request, course_id):
@@ -82,22 +144,6 @@ def remove_lesson(request, id):
     else:
         return render(request, 'errors/unauthorized.html', status=401)
 
-
-@login_required
-def subscribe_on_course(request, course_id):
-    course = get_object_or_404(models.Course, pk=course_id)
-    if request.user in course.participants.all():
-        return render(request, 'errors/already_in.html', status=400)
-    if request.method == "POST":
-        form = forms.SubscribeForm(request.POST)
-        if(form.is_valid()):
-            key = form.cleaned_data['course_key']
-            if key == course.access_key:
-                course.participants.add(request.user)
-                return redirect('course', id=course.id)
-            else:
-                return render(request, 'errors/wrong_key.html', status=400)
-
 @login_required
 def lesson_check_in(request, lesson_id):
     lesson = get_object_or_404(models.Lesson, pk=lesson_id)
@@ -114,51 +160,3 @@ def lesson_check_in(request, lesson_id):
                 return redirect('course', id=lesson.course.id)
             else:
                 return render(request, 'errors/wrong_key.html', status=400)
-
-@login_required
-def course_report(request, id):
-    course = get_object_or_404(models.Course, pk=id)
-    if request.user == course.owner:
-        participants = course.participants.all()
-        lessons = course.lesson_set.all()
-        ctx = {'participants': participants, 'lessons': lessons, 'course':course}
-        return render(request, 'classes/course_report.html', context = ctx)
-    else:
-        return render(request, 'errors/unauthorized.html', status=401)
-
-
-@login_required
-def new_course(request):
-    if request.method == "POST":
-        form = forms.CourseCreation(request.POST)
-        if(form.is_valid()):
-            course = form.save(commit=False)
-            course.owner = request.user
-            course.save()
-            return redirect('course', id=course.id)
-    else:
-        form = forms.CourseCreation()
-        return render(request, 'classes/create_course.html', context={'form':form})
-
-@login_required
-def my_courses(request):
-    owned_courses = request.user.owner.all()
-    subscribed_courses = request.user.participants.all()
-    ctx = {"owned": owned_courses, "subscribed": subscribed_courses}
-    return render(request, 'classes/my_courses.html', context=ctx)
-
-
-@login_required
-def delete_course(request, id):
-    course = get_object_or_404(models.Course, pk=id)
-    if request.user == course.owner:
-        course.delete()
-        return redirect('my_courses')
-    else:
-        return render(request, 'errors/unauthorized.html', status=401)
-
-@login_required
-def unsubscribe(request, id):
-    course = get_object_or_404(models.Course, pk=id)
-    course.participants.remove(request.user)
-    return redirect('my_courses')
